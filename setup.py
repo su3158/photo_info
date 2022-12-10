58d6bfb17ae6eb1281685be031f6268371a55a6c
su3158 <su3158@protonmail.com>
2022-12-11 00:15:49 +0900

    new setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='photo_info',
+    version='0.3',
+    description='',
+    author='su3158',
+    author_email='su3158@protonmail.com',
+    url='',
+)
